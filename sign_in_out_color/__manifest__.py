cbbdf37b2b667f94d039183dea229a353d50a1f4
Karthik, Sodexis <karthik@sodexis.com>
2017-05-08 11:51:39 +0530

    [ID:2567] Investigate Odoo Paid Apps

---
+++ sign_in_out_color/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Sign in and Sign out color change",
+    'description': """
+        This module is used to change the colors of
+        Sign in and Sign out buttons in Attendance.
+    """,
+    'author': "Sodexis, Inc <dev@sodexis.com>",
+    'website': "http://sodexis.com/",
+    'category': 'Tiny Apps',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+        'hr_attendance'
+    ],
+    'data': [
+        'views/sign_in_out_view.xml'
+    ],
+    'installable': True,
+}
